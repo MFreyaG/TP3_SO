9c7109f939d9477befc4fc8255cf3b03ae9200c5
Cesar Morais <cesarpmorais@hotmail.com>
2024-08-07 23:26:34 -0300

    Added structs to pager.c

--- src/pager.c
+++ src/pager.c
@@ -1,4 +1,41 @@
+#include <pthread.h>
+
 #include "pager.h"
+
+
+// Structs used by the professor @ https://gitlab.dcc.ufmg.br/cunha-dcc605/mempager-assignment/-/blob/master/src/pager.c 
+struct frame_data {
+	pid_t pid;
+	int page;
+	int prot; /* PROT_READ (clean) or PROT_READ | PROT_WRITE (dirty) */
+	int dirty; /* prot may be reset by pager_free_frame() */
+};
+
+struct page_data {
+	int block;
+	int on_disk; /* 0 indicates page was written to disk */
+	int frame; /* -1 indicates non-resident */
+};
+
+struct proc {
+	pid_t pid;
+	int npages;
+	int maxpages;
+	struct page_data *pages;
+};
+
+struct pager {
+	pthread_mutex_t mutex;
+	int nframes;
+	int frames_free;
+	int clock;
+	struct frame_data *frames;
+	int nblocks;
+	int blocks_free;
+	pid_t *block2pid;
+	struct proc **pid2proc;
+};
+
 
 void pager_init(int nframes, int nblocks){
 
