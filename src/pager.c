aa8efa1697ee06839ad5774c320fc5315498c773
Matheus Grandinetti <matheusgrandinetti@gmail.com>
2024-08-07 22:54:18 -0300

    initial setup

---
+++ src/pager.c
@@ -0,0 +1,25 @@
+#include "pager.h"
+
+void pager_init(int nframes, int nblocks){
+
+}
+
+void pager_create(pid_t pid){
+
+}
+
+void *pager_extend(pid_t pid){
+    
+}
+
+void pager_fault(pid_t pid, void *addr){
+
+}
+
+int pager_syslog(pid_t pid, void *addr, size_t len){
+    return 1;
+}
+
+void pager_destroy(pid_t pid){
+
+}
